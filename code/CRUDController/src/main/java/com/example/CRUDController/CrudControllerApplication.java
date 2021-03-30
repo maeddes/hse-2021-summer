99102bf6bc241a48bffe3e1e06fa01209c0f56b7
Matthias Haeussler <maeddes@gmail.com>
2021-03-30 09:19:38 +0200

    CRUD Controller Microservice

---
+++ code/CRUDController/src/main/java/com/example/CRUDController/CrudControllerApplication.java
@@ -0,0 +1,67 @@
+package com.example.CRUDController;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class CrudControllerApplication {
+
+	ArrayList<String> listOfStrings = new ArrayList<String>();
+	private Logger logger = LoggerFactory.getLogger(CrudControllerApplication.class);
+
+	@GetMapping("/strings")
+	public String readAllStrings(){
+
+		logger.info("Strings being returned {}",listOfStrings);
+		return listOfStrings.toString();
+
+	}
+
+	@PostMapping("/strings/{newString}")
+	public String createString(@PathVariable String newString){
+
+		logger.error("String to add: {}",newString);
+		listOfStrings.add(newString);
+		logger.debug("Current list of strings {}",listOfStrings);
+		return newString+" has been added to the list";
+
+	}
+
+	@PutMapping("/strings/{oldString}/{newString}")
+	public String updateString(@PathVariable String newString){
+
+		// Todo :-)
+		return "nothing happened with "+newString;
+
+	}
+
+	@DeleteMapping("/strings/{stringToDelete}")
+	public String deleteString(@PathVariable String stringToDelete){
+
+		logger.info("String to delete : {}",stringToDelete);
+		listOfStrings.remove(stringToDelete);
+		logger.debug("Current list of strings {}",listOfStrings);
+		return stringToDelete+" has most likely been removed from the list";
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudControllerApplication.class, args);
+	}
+
+}
---
+++ code/CRUDController/src/test/java/com/example/CRUDController/CrudControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
