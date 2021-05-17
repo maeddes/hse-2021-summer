4b34bd653e4787232516945dd4d29d6f1122fd7c
Matthias Haeussler <maeddes@gmail.com>
2021-05-17 17:02:58 +0200

    Config Server and Client

--- code/HSEHelloWorld/src/main/java/com/example/HSEHelloWorld/HseHelloWorldApplication.java
+++ code/HSEHelloWorld/src/main/java/com/example/HSEHelloWorld/HseHelloWorldApplication.java
@@ -1,7 +1,9 @@
 package com.example.HSEHelloWorld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,9 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class HseHelloWorldApplication {
 
 	private String state = "no_state";
+
+	@Value("${config.source:unkown}")
+	private String configVariable;
+
+	@GetMapping("/config")
+	public String getConfigSource(){
+
+		return "Configuration Source: "+configVariable;
+
+	}
 
 	@GetMapping
 	public String sayHello(){
