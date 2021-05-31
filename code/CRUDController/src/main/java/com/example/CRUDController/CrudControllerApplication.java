63bc9d3a3141e1a661430ab39f0f0e2e55a68041
Matthias Haeussler <maeddes@gmail.com>
2021-05-31 13:35:20 +0200

    patches

--- code/CRUDController/src/main/java/com/example/CRUDController/CrudControllerApplication.java
+++ code/CRUDController/src/main/java/com/example/CRUDController/CrudControllerApplication.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,9 +14,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+// @Configuration
+// @EnableAutoConfiguration
+// @ComponentScan
+@SpringBootApplication
 public class CrudControllerApplication {
 
 	ArrayList<String> listOfStrings = new ArrayList<String>();
