004e0c9762694ba945cb01128845d6d910614c3a
Matthias Haeussler <maeddes@gmail.com>
2021-06-07 16:26:22 +0200

    MVP

--- code/ThymeleafUI/src/main/java/com/example/ThymeleafUI/ThymeleafController.java
+++ code/ThymeleafUI/src/main/java/com/example/ThymeleafUI/ThymeleafController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class ThymeleafController {
@@ -16,24 +17,37 @@ public class ThymeleafController {
     @Value("${welcome.recipient}")
 	private String message;
 
-	//private List<String> stringList = Arrays.asList("one","two","three");
-	private ArrayList<String> stringList = new ArrayList<String>();
+	//private List<String> stringList =nt Arrays.asList("one","two","three");
+	private ArrayList<String> todoList;
 
 	@GetMapping("/")
 	public String displayPage(Model model){
 
+        Todo[] todos = WebClient
+            .create("http://localhost:8080/todos/")
+            .get()
+            .retrieve()
+            .bodyToMono(Todo[].class)
+            .block();
+
+        todoList = new ArrayList<String>();
+
+        for(int i = 0; i < todos.length; i++)
+            todoList.add(todos[i].getName());
+
 		model.addAttribute("message", message);
-		model.addAttribute("list", stringList);
+		model.addAttribute("list", todoList);
 
 		return "page";
 	}
 
-	@GetMapping("/{name}")
+	//@GetMapping("/{name}")
 	public String displayPage(@PathVariable String name, Model model){
 
 		if(name != null && name.length() != 0) message = name;
+
 		model.addAttribute("message", message);
-		model.addAttribute("list", stringList);
+		model.addAttribute("list", todoList);
 
 		return "page";
 	}
@@ -42,14 +56,36 @@ public class ThymeleafController {
 	public String addItem(@RequestParam String newitem, Model model){
 
 		System.out.println("New item: "+newitem);
-		if(!newitem.equals("")){
-			stringList.add(newitem);
-		}
-		//message = newitem;
-		model.addAttribute("message", message);
-		model.addAttribute("list", stringList);
 
-		return "page";
+        WebClient
+            .create("http://localhost:8080/todos/"+newitem)
+            .post()
+            .retrieve()
+            .bodyToMono(Todo.class)
+            .block();
+
+        return "redirect:/";
+	}
+
+    @PostMapping("/delete")
+	public String deleteItem(@RequestParam String olditem, Model model){
+
+		System.out.println("Ol to delete  item: "+olditem);
+        WebClient
+            .create("http://localhost:8080/todos/"+olditem)
+            .delete()
+            .retrieve()
+            .bodyToMono(Void.class)
+            .block();
+
+            // WebClient
+            // .create("http://localhost:8080/todos/"+oldItem)
+            // .delete()
+            // .retrieve()
+            // .bodyToMono(Void.class)
+            // .block();
+
+        return "redirect:/";
 	}
 
 	// @GetMapping("/strings")
