cfccfff868e8d2c4951184e170cd18e5e56157a3
Matthias Haeussler <maeddes@gmail.com>
2021-06-07 15:59:48 +0200

    WebClient Playground Stuff

---
+++ code/ThymeleafUI/src/main/java/com/example/ThymeleafUI/ThymeleafController.java
@@ -0,0 +1,62 @@
+package com.example.ThymeleafUI;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ThymeleafController {
+
+    @Value("${welcome.recipient}")
+	private String message;
+
+	//private List<String> stringList = Arrays.asList("one","two","three");
+	private ArrayList<String> stringList = new ArrayList<String>();
+
+	@GetMapping("/")
+	public String displayPage(Model model){
+
+		model.addAttribute("message", message);
+		model.addAttribute("list", stringList);
+
+		return "page";
+	}
+
+	@GetMapping("/{name}")
+	public String displayPage(@PathVariable String name, Model model){
+
+		if(name != null && name.length() != 0) message = name;
+		model.addAttribute("message", message);
+		model.addAttribute("list", stringList);
+
+		return "page";
+	}
+
+	@PostMapping("/")
+	public String addItem(@RequestParam String newitem, Model model){
+
+		System.out.println("New item: "+newitem);
+		if(!newitem.equals("")){
+			stringList.add(newitem);
+		}
+		//message = newitem;
+		model.addAttribute("message", message);
+		model.addAttribute("list", stringList);
+
+		return "page";
+	}
+
+	// @GetMapping("/strings")
+	// public String displayPageWithStrings(Model model){
+
+	// 	model.addAttribute("list", stringList);
+	// 	return "page";
+	// }
+    
+}
